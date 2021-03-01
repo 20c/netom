bce907a7237565ff8f73e300aacd8a3ce49618da
Matt Griswold <grizz@20c.com>
2021-03-01 13:52:55 -0600

    convert to poetry

--- src/netom/__main__.py
+++ src/netom/__main__.py
@@ -1,42 +1,7 @@
-
-import argparse
-import io
 import sys
 
-import munge
 
-import netom
+if __name__ == "__main__":
+    from .cli import main
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-
-    parser = argparse.ArgumentParser(description="netom")
-    parser.add_argument("--version", action="version",
-                        version="{}s version {}".format("%(prog)", netom.__version__))
-    # parser.add_argument("--output-format", help="output format (yaml, json, text)")
-
-    parser.add_argument("command")
-    parser.add_argument("render_call")
-    parser.add_argument("model_version")
-    parser.add_argument("model_type")
-    parser.add_argument("data_file")
-    args = parser.parse_args(argv)
-
-    # get dict of options and update config
-    argd = vars(args)
-
-    command = argd["command"]
-    render_call = argd["render_call"]
-    model_version = argd["model_version"]
-    model_type = argd["model_type"]
-
-    render = netom.Render(model_version, model_type)
-    data = munge.load_datafile(argd["data_file"])
-
-    with io.StringIO() as fobj:
-        getattr(render, render_call)(data, fobj)
-        print("---")
-        print(fobj.getvalue())
-
-    return 0
+    sys.exit(main())
